e1fa497355610a435ff56be1e5c0582d64053970
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-20 21:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+            const colorSelect = document.getElementById("colorSelect");
+            colorSelect.remove(colorSelect.selectedIndex);
+}
